2821662aab45cb4b83633200df36a3f48a2a1ac7
Kauani <0000107880784xsp@al.educacao.sp.gov.br>
2024-11-21 09:06:05 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Matemática',
+    'Como a matemática esta presente em nossa vida?',
+    'Ir as compras, preparar uma receita, brincar e jogar, contar o tempo no relógio, na música, etc'
+)
+
+criaCartao(
+    'História',
+    'Quem inventou o termo história?',
+    'O termo "história" é originado do vocábulo grego histories'
+)
+
+criaCartao(
+    'Inglês',
+    'Porque inglês é a lingua universal?',
+    'Além da dominação geográfica e cultural, outro fator que foi determinante para que o inglês se estabelecesse como língua universal foi a facilidade de compreensão e aprendizado do idioma.'
+)
+
+criaCartao(
+    'Educação Financeira',
+    'O que é ter uma educação financeira?',
+    'A educação financeira é definida como o processo mediante o qual “os indivíduos e as sociedades melhoram a sua compreensão em relação aos conceitos e produtos financeiros, de maneira que, com informação, formação e orientação, possam desenvolver os valores e as competências necessários para se tornarem ...'
+)
